a3a36ecc0c8ed026b4c6fda1a743f317e4e2e1b8
Janko Diminic <janko.diminic@sedamit.hr>
2019-09-26 13:25:35 +0200

    Initial commit

---
+++ web/src/main/java/hr/ja/security/web/MainView.java
@@ -0,0 +1,29 @@
+package hr.ja.security.web;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.dom.Style;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        H1 heading = new H1("Ovo je h1 janko");
+
+        Label greeting = new Label("label neki");
+
+        add(heading, greeting);
+    }
+
+}
