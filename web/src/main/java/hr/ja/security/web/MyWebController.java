f7ce3a02f44ba14af9675657794a9a2036abcd2f
Janko Diminic <janko.diminic@sedamit.hr>
2019-10-21 09:59:08 +0200

    save all

--- web/src/main/java/hr/ja/security/web/MyWebController.java
+++ web/src/main/java/hr/ja/security/web/MyWebController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,36 +17,52 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class MyWebController {
 
-    @RequestMapping("/")
-    public String home(Principal principal) {
-        log.debug("user {}", principal);
-        return "index";
-    }
+	@RequestMapping("/")
+	public String home(Principal principal) {
+		log.debug("user {}", principal);
 
-    @GetMapping(path = "/logout")
-    public String logout(HttpServletRequest request) throws ServletException {
-        request.logout();
-        return "redirect:/";
-    }
+		getID(principal);
 
-    @Secured("ROLE_WEB")
-    @RequestMapping("/protected_web")
-    public String protectedWeb(Principal principal) {
-        log.debug("user {}", principal);
-        return "protected_web";
-    }
+		return "index";
+	}
 
-    @Secured("ROLE_ADMIN")
-    @RequestMapping("/admin_page")
-    public String adminPage(Principal principal) {
-        log.debug("Admin user {}", principal);
-        return "admin_page1";
-    }
+	private void getID(Principal principalReal) {
+		try {
+			KeycloakAuthenticationToken principal = (KeycloakAuthenticationToken) principalReal; // (KeycloakAuthenticationToken)
+																									// request.getUserPrincipal();
 
-    // protected by annotation....in SecurityConfig
-    @RequestMapping("/web1")
-    public String web1(Principal principal) {
-        log.debug("user {}", principal);
-        return "web1";
-    }
+			String userId = principal.getAccount().getKeycloakSecurityContext().getIdToken().getSubject();
+
+			log.debug("User ID " + userId);
+		} catch (Exception e) {
+			log.error("", e);
+		}
+	}
+
+	@GetMapping(path = "/logout")
+	public String logout(HttpServletRequest request) throws ServletException {
+		request.logout();
+		return "redirect:/";
+	}
+
+	@Secured("ROLE_WEB")
+	@RequestMapping("/protected_web")
+	public String protectedWeb(Principal principal) {
+		log.debug("user {}", principal);
+		return "protected_web";
+	}
+
+	@Secured("ROLE_ADMIN")
+	@RequestMapping("/admin_page")
+	public String adminPage(Principal principal) {
+		log.debug("Admin user {}", principal);
+		return "admin_page1";
+	}
+
+	// protected by annotation....in SecurityConfig
+	@RequestMapping("/web1")
+	public String web1(Principal principal) {
+		log.debug("user {}", principal);
+		return "web1";
+	}
 }
